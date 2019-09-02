6199a3509efb4241a28ec8cb08f4c4a9fb0eae2d
syedishmam <syedishmam17@gmail.com>
2019-09-02 19:46:57 -0400

    Add Google OAuth credentials

--- battleships_game/src/components/GoogleAuth.js
+++ battleships_game/src/components/GoogleAuth.js
@@ -1,6 +1,17 @@
+//AIzaSyAKht5SI8YDu-uDjfz24okm7Y8rcLc3yes
+
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '1060584970583-k247tknjfq2qp4lm9ob0k0qbhj7svtjm.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
+
     render() {
         return (
             <div>GoogleAuth</div>
