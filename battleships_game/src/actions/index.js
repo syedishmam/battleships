309e1fc7d19be4fc3523d41cd9808e46d61f0ce8
niaz151 <niaz151@gmail.com>
2019-09-03 18:30:53 -0400

    Edit Tile and Grid CSS

--- battleships_game/src/actions/index.js
+++ battleships_game/src/actions/index.js
@@ -10,3 +10,10 @@ export const signOut = () => {
         type: 'SIGN_OUT'
     }
 }
+
+export const shootTile = ({x,y}) => {
+  return {
+    type: 'SHOOT_TILE',
+    payload: {x,y}
+  }
+}
--- battleships_game/src/components/Tile.js
+++ battleships_game/src/components/Tile.js
@@ -3,10 +3,13 @@ import '../styles/tile.css'
 
 class Tile extends React.Component{
 
+  onClick = () =>{
+    
+  }
 
   render(){
     return(
-      <div className="tile">
+      <div className="tile" onClick={this.onClick}>
        
       </div>
     )
