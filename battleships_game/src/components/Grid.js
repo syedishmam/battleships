bf660440d2cfa1636df99efd0295a1372aecc51d
niaz151 <niaz151@gmail.com>
2019-09-02 22:34:04 -0400

    Added Initial State

---
+++ battleships_game/src/components/Grid.js
@@ -0,0 +1,17 @@
+import React from React;
+import Tile from './Tile';
+
+class Grid extends React.Component{
+
+
+
+  render(){
+    <div>
+      Grid
+    </div>
+  }
+
+
+}
+
+export default Grid;
---
+++ battleships_game/src/components/Tile.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Tile extends React.Component{
+
+
+  render(){
+    <div>
+      tile
+    </div>
+  }
+}
+
+export default Tile;
