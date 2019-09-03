a95c006e96b7d97443ee2fde3a97e14400c9b865
syedishmam <syedishmam17@gmail.com>
2019-09-03 18:42:47 -0400

    Remove initial state

--- battleships_game/src/index.js
+++ battleships_game/src/index.js
@@ -6,66 +6,8 @@ import {createStore} from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-const initial_state = {
-  players:[
-    {
-      username:'p_one',
-      previous_move: [0,0],
-      ships:[
-        {
-          name:'carrier_one',
-          alive: true,
-          position:'vertical',
-          coordinates: [
-            [1,2],
-            [1,3],
-            [1,4]
-          ]
-        },
-        {
-          name:'frigate_one',
-          alive: true,
-          position:'horizontal',
-          coordinates: [
-            [1,6],
-            [2,6],
-            [3,6]
-          ]
-        }
-      ]  
-    },
-    {
-      username:'p_two',
-      previous_move: [1,1],
-      ships:[
-        {
-          name:'carrier_two',
-          alive: true,
-          position:'vertical',
-          coordinates: [
-            [4,7],
-            [4,8],
-            [4,9]
-          ]
-        },
-        {
-          name:'frigate_two',
-          alive: true,
-          position:'horizontal',
-          coordinates: [
-            [4,6],
-            [5,6],
-            [6,6]
-          ]
-        }
-      ]  
-    }
-  ]
-}
-
 const store = createStore(
-    reducers,
-    initial_state
+    reducers
 )
 
 console.log(store.getState())
@@ -76,4 +18,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-
