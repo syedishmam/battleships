7018dfe47c7c731bdf8e0e5dfb6439e80ae505bb
syedishmam <syedishmam17@gmail.com>
2019-09-03 18:44:12 -0400

    Handles sign in, sign out actions

---
+++ battleships_game/src/reducers/signInAndOutReducer.js
@@ -0,0 +1,15 @@
+export default (state = {
+    isSignedIn: null,
+    userId: null
+}, action) => {
+    switch(action.type) {
+        case 'SIGN_IN':
+            return {...state, isSignedIn: true, userId: action.payload};
+        
+        case 'SIGN_OUT': 
+            return {...state, isSignedIn: false, userId: null}
+        
+        default: 
+            return state;
+    }
+}
