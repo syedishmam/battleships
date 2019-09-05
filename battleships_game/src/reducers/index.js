a61f564cbdecdfd5c1e818f424102f3499ddd9b2
syedishmam <syedishmam17@gmail.com>
2019-09-05 19:32:57 -0400

    Change guest to isGuest

--- battleships_game/src/reducers/index.js
+++ battleships_game/src/reducers/index.js
@@ -3,9 +3,11 @@ import {combineReducers} from 'redux';
 import signInAndOutReducer from './signInAndOutReducer';
 import playersReducer from './playersReducer';
 import shootTileReducer from './shootTileReducer';
+import continueAsGuest from './continueAsGuestReducer';
 
 export default combineReducers({
     players: playersReducer,
     user: signInAndOutReducer,
-    shootTile: shootTileReducer
+    shootTile: shootTileReducer,
+    isGuest: continueAsGuest
 });
