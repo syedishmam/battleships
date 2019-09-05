ebf3723f0535d5b68aa088304c5b833c2441e1cb
syedishmam <syedishmam17@gmail.com>
2019-09-05 15:49:03 -0400

    Route component from GoogleAuth to LoginPage

--- battleships_game/src/components/App.js
+++ battleships_game/src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Router, Route} from 'react-router-dom';
 
-import GoogleAuth from './login-page/GoogleAuth';
 import Grid from './Grid';
 import history from '../history'
 import '../styles/app.css'
+import LoginPage from './login-page/LoginPage';
 
 function App() {
   return (
     <Router history={history}>
-      <Route path="/login" component={GoogleAuth}/>
+      <Route path="/login" component={LoginPage}/>
         <div className="grid-container">
           <Route path="/game" component={() => <Grid type="Opponent"/>} />
           <Route path="/game" component={() => <Grid type="You" />} />
