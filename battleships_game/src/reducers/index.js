8f9c0bd3b002dd50b8f60bf81a6fd9850898fc66
syedishmam <syedishmam17@gmail.com>
2019-09-06 16:39:06 -0400

    Add fetchStatsReducer to combineReducers

--- battleships_game/src/reducers/index.js
+++ battleships_game/src/reducers/index.js
@@ -4,10 +4,12 @@ import signInAndOutReducer from './signInAndOutReducer';
 import playersReducer from './playersReducer';
 import shootTileReducer from './shootTileReducer';
 import continueAsGuest from './continueAsGuestReducer';
+import fetchStatsReducer from './fetchStatsReducer';
 
 export default combineReducers({
     players: playersReducer,
     user: signInAndOutReducer,
     shootTile: shootTileReducer,
-    isGuest: continueAsGuest
+    isGuest: continueAsGuest,
+    stats: fetchStatsReducer
 });
