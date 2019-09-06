5ee9b34c648b9374abe3c9319ea8f38175f7facd
syedishmam <syedishmam17@gmail.com>
2019-09-06 17:04:50 -0400

    Create getPlayerStats

--- battleships_game/src/components/stats/player-stats/PlayerStats.js
+++ battleships_game/src/components/stats/player-stats/PlayerStats.js
@@ -6,53 +6,67 @@ import {fetchPlayerStats} from '../../../actions';
 import './PlayerStats.css';
 
 class PlayerStats extends React.Component {
-    getPlayerStats(userId) {
-        this.props.fetchPlayerStats(userId);
+    getPlayerStats() {
+        this.props.fetchPlayerStats(this.props.userId);
+    }
+
+    renderStatsTable() {
+        this.getPlayerStats();
+        return (
+            <table className="text-center">
+                <tbody>
+                    <tr>
+                        <th>Stat</th>
+                        <th>Score</th>
+                    </tr>
+                    <tr>
+                        <th>Wins</th>
+                        <th>5</th>
+                    </tr>
+                    <tr>
+                        <th>Losses</th>
+                        <th>3</th>
+                    </tr>
+                    <tr>
+                        <th>WLR</th>
+                        <th>1.66</th>
+                    </tr>
+                    <tr>
+                        <th>Ships Destroyed</th>
+                        <th>14</th>
+                    </tr>
+                    <tr>
+                        <th>Hits</th>
+                        <th>35</th>
+                    </tr>
+                    <tr>
+                        <th>Misses</th>
+                        <th>54</th>
+                    </tr>
+                    <tr>
+                        <th>HMR</th>
+                        <th>0.64</th>
+                    </tr>
+                </tbody>
+        </table>
+        )
     }
 
     render() {
         return (
             <div>
                 <div className="display-1 title text-center">Player Stats</div>
-                <table className="text-center">
-                    <tbody>
-                        <tr>
-                            <th>Stat</th>
-                            <th>Score</th>
-                        </tr>
-                        <tr>
-                            <th>Wins</th>
-                            <th>5</th>
-                        </tr>
-                        <tr>
-                            <th>Losses</th>
-                            <th>3</th>
-                        </tr>
-                        <tr>
-                            <th>WLR</th>
-                            <th>1.66</th>
-                        </tr>
-                        <tr>
-                            <th>Ships Destroyed</th>
-                            <th>14</th>
-                        </tr>
-                        <tr>
-                            <th>Hits</th>
-                            <th>35</th>
-                        </tr>
-                        <tr>
-                            <th>Misses</th>
-                            <th>54</th>
-                        </tr>
-                        <tr>
-                            <th>HMR</th>
-                            <th>0.64</th>
-                        </tr>
-                    </tbody>
-                </table>
+                {this.renderStatsTable()}
             </div>
         )
     }
 }
 
-export default connect(null, {fetchPlayerStats})(PlayerStats);
+const mapStateToProps = (state) => {
+    return {
+        userId: state.user.userId,
+        currentPlayerStates: state.stats.currentPlayerStates
+    }
+}
+
+export default connect(mapStateToProps, {fetchPlayerStats})(PlayerStats);
