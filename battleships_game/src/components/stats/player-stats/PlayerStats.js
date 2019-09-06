ff1c34bba9cf36ffdad66b55b417ec4846ce2a72
syedishmam <syedishmam17@gmail.com>
2019-09-06 15:05:57 -0400

    Class component boiler plate

---
+++ battleships_game/src/components/stats/player-stats/PlayerStats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class PlayerStats extends React.Component {
+    render() {
+        return (
+            <div>PlayerStats</div>
+        )
+    }
+}
+
+export default PlayerStats;
