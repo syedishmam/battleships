831e5e53e239493ddf8a5e7261e83ec591045510
syedishmam <syedishmam17@gmail.com>
2019-09-06 15:04:45 -0400

    Import stylesheet, edit Link paths

--- battleships_game/src/components/main-menu/MenuOptions.js
+++ battleships_game/src/components/main-menu/MenuOptions.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import './LoginStyles.css';
+
 class MenuOptions extends React.Component {
     render() {
         return (
             <div>
-                <Link to={`/game`} className="btn btn-primary">vs AI</Link> <br/>
-                <Link to={`/game`} className="btn btn-primary">vs Player</Link> <br/>
-                <Link to={`/game`} className="btn btn-primary">Your Stats</Link> <br/>
-                <Link to={`/game`} className="btn btn-primary">Global Stats</Link>
+                <Link to={`/game`} className="btn btn-primary menuButton">vs A.I</Link> <br/>
+                <Link to={`/game`} className="btn btn-primary menuButton">vs Player</Link> <br/>
+                <Link to={`/player-stats`} className="btn btn-primary menuButton">Your Stats</Link> <br/>
+                <Link to={`/global-stats`} className="btn btn-primary menuButton">Global Stats</Link>
             </div>
         )
     }
