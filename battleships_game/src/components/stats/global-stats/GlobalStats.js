813abbc56bd51da0d027b6b4755de2f3f5e2bc8d
syedishmam <syedishmam17@gmail.com>
2019-09-07 16:24:31 -0400

    Add table styles

--- battleships_game/src/components/stats/global-stats/GlobalStats.js
+++ battleships_game/src/components/stats/global-stats/GlobalStats.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
+import './GlobalStats.css';
+
 class GlobalStats extends React.Component {
+    renderStatsTable() {
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <th colSpan="2">Rankings</th>
+                    </tr>
+                    <tr>
+                        <th className="rankingNumericalPosition">1.</th>
+                        <th><a href="/">Ish</a></th>
+                    </tr>
+                    <tr>
+                        <th className="rankingNumericalPosition">2.</th>
+                        <th><a href="/">Niaz</a></th>
+                    </tr>
+                    <tr>
+                        <th className="rankingNumericalPosition">3.</th>
+                        <th><a href="/">Bob</a></th>
+                    </tr>
+                    <tr>
+                        <th className="rankingNumericalPosition">4.</th>
+                        <th><a href="/">Tom</a></th>
+                    </tr>
+                    <tr>
+                        <th className="rankingNumericalPosition">5.</th>
+                        <th><a href="/">Bill</a></th>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+
     render() {
         return (
-            <div>GlobalStats</div>
+            <div>
+                <div className="display-1 title text-center">Global Stats</div>
+                {this.renderStatsTable()}
+            </div>
         )
     }
 }
