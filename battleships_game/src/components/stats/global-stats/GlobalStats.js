031250c5fdba36df769de9c4d5c5603f79881b9e
syedishmam <syedishmam17@gmail.com>
2019-09-07 14:59:46 -0400

    Class component boiler plate

---
+++ battleships_game/src/components/stats/global-stats/GlobalStats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class GlobalStats extends React.Component {
+    render() {
+        return (
+            <div>GlobalStats</div>
+        )
+    }
+}
+
+export default GlobalStats;
