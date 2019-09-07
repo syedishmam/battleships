da46dd702c9a0a4e228723cb5cb9e893cea5be03
niaz151 <niaz151@gmail.com>
2019-09-07 18:13:02 -0400

    Render Two Grids From One Route

--- battleships_game/src/components/App.js
+++ battleships_game/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Router, Route, Switch} from 'react-router-dom';
 
-import Grid from './Grid';
+import Grids from './Grids';
 import history from '../history'
 import '../styles/app.css'
 import Menu from './main-menu/Menu';
@@ -12,8 +12,7 @@ function App() {
     <Router history={history}>
       <Switch>
         <Route path="/menu" component={Menu}/>
-        <Route path="/game" component={() => <Grid type="Opponent"/>} />
-        <Route path="/game" component={() => <Grid type="You" />} />
+        <Route path="/game" component={Grids} />
         <Route path="/player-stats/:userId" component={PlayerStats} />
       </Switch>
     </Router>
---
+++ battleships_game/src/components/Grids.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Tile from './Tile';
+import '../styles/grid.css';
+
+class Grids extends React.Component{
+  
+  makeGrid(type){
+
+    let tiles = []
+    
+    for(var i = 0; i < 48; i ++){
+      this.props.type === 'Opponent' ? 
+      (tiles.push(<Tile type="Opponent" num={i} key={i} />)):
+      (tiles.push(<Tile type="You" num={i} key={i} />))
+    }
+
+    return tiles
+  }
+
+  render()
+  {
+    var grid_one = this.makeGrid('Opponent')
+    var grid_two = this.makeGrid('You')
+    
+    return(
+      <div className="d-flex flex-column align-items-center justify-content-center grid-container">
+        <div className="grid">
+          {grid_one} 
+        </div>
+        <div className="grid">
+          {grid_two} 
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Grids;
