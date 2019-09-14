fdcc5b875bc585df176dc15553d32a9d032b510f
syedishmam <syedishmam17@gmail.com>
2019-09-14 16:51:13 -0400

    Rankings show "..." until rankings state updates

--- battleships_game/src/components/stats/global-stats/GlobalStats.js
+++ battleships_game/src/components/stats/global-stats/GlobalStats.js
@@ -34,35 +34,67 @@ class GlobalStats extends React.Component {
     }
 
     renderStatsTable() {
-        return (
-            <table>
-                <tbody>
-                    <tr>
-                        <th colSpan="2">Rankings</th>
-                    </tr>
-                    <tr>
-                        <th className="rankingNumericalPosition">1.</th>
-                        <th><Link to={`/player-stats/`}>Ish</Link></th>
-                    </tr>
-                    <tr>
-                        <th className="rankingNumericalPosition">2.</th>
-                        <th><a href="/">Niaz</a></th>
-                    </tr>
-                    <tr>
-                        <th className="rankingNumericalPosition">3.</th>
-                        <th><a href="/">Bob</a></th>
-                    </tr>
-                    <tr>
-                        <th className="rankingNumericalPosition">4.</th>
-                        <th><a href="/">Tom</a></th>
-                    </tr>
-                    <tr>
-                        <th className="rankingNumericalPosition">5.</th>
-                        <th><a href="/">Bill</a></th>
-                    </tr>
-                </tbody>
-            </table>
-        )
+        if(this.state.rankings === null) {
+            return (
+                <table>
+                    <tbody>
+                        <tr>
+                            <th colSpan="2">Rankings</th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">1.</th>
+                            <th>...</th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">2.</th>
+                            <th>...</th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">3.</th>
+                            <th>...</th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">4.</th>
+                            <th>...</th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">5.</th>
+                            <th>...</th>
+                        </tr>
+                    </tbody>
+                </table>
+            )
+        } else {
+            return (
+                <table>
+                    <tbody>
+                        <tr>
+                            <th colSpan="2">Rankings</th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">1.</th>
+                            <th><Link to={`/player-stats/`}>{this.state.rankings[0].userName}</Link></th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">2.</th>
+                            <th><Link to={`/player-stats/`}>{this.state.rankings[1].userName}</Link></th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">3.</th>
+                            <th><Link to={`/player-stats/`}>{this.state.rankings[2].userName}</Link></th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">4.</th>
+                            <th><Link to={`/player-stats/`}>{this.state.rankings[3].userName}</Link></th>
+                        </tr>
+                        <tr>
+                            <th className="rankingNumericalPosition">5.</th>
+                            <th><Link to={`/player-stats/`}>{this.state.rankings[4].userName}</Link></th>
+                        </tr>
+                    </tbody>
+                </table>
+            )
+        }
     }
 
     render() {
