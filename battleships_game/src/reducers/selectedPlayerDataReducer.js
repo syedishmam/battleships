ab221b0fdca1e94027cb5b1f29eeb39f8c757a93
syedishmam <syedishmam17@gmail.com>
2019-09-17 18:08:44 -0400

    Change player to data

--- battleships_game/src/reducers/selectedPlayerDataReducer.js
+++ battleships_game/src/reducers/selectedPlayerDataReducer.js
@@ -2,7 +2,7 @@ export default (state = {
 }, action) => {
     switch(action.type) {
         case 'STORE_SELECTED_PLAYER_DATA':
-            return {...state, player: action.payload};
+            return {...state, data: action.payload};
 
         default:
             return state;
