d1547c6db278dfcb22221f3b8466a96ffaff239d
syedishmam <syedishmam17@gmail.com>
2019-09-17 18:22:07 -0400

    Pass title prop to PlayerStats

--- battleships_game/src/components/stats/player-stats/YourStats.js
+++ battleships_game/src/components/stats/player-stats/YourStats.js
@@ -66,7 +66,7 @@ class YourStats extends React.Component {
 
     render() {
         return (
-            <PlayerStats renderTable={this.renderStatsTable} />
+            <PlayerStats renderTable={this.renderStatsTable} title="Your Stats"/>
         )
     }
 }
