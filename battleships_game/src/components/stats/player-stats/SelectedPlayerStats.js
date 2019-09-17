85c299accd9f38910804c88be33f1b949e8006bc
syedishmam <syedishmam17@gmail.com>
2019-09-17 17:38:33 -0400

    Class boiler plate

---
+++ battleships_game/src/components/stats/player-stats/SelectedPlayerStats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SelectedPlayerStats extends React.Component {
+    render() {
+        return (
+            <div>SelectedPlayerStats</div>
+        )
+    }
+}
+
+export default SelectedPlayerStats;
